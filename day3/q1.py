a7f1925311f6c22013233a94a72fb01156b58e57
Julius <q1541599745@gmail.com>
2022-12-16 12:43:57 +0000

    day3 added

--- day3/q1.py
+++ day3/q1.py
@@ -3,3 +3,18 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 file = open(os.path.join(__location__,'input.txt'), 'r',encoding="utf-8")
 
+sum=0
+while 1:
+    line = str(file.readline().replace("\n",""))
+    if not line:
+        break
+    first=line[0:int(len(line)/2)]
+    second=line[int(len(line)/2):int(len(line))]
+    for i in first:
+        if i in second:
+            if ord(i)>96:
+                sum+=ord(i)-96
+            else:
+                sum+=ord(i)-64+26
+            break
+print(sum)
--- day3/q2.py
+++ day3/q2.py
@@ -3,3 +3,18 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 file = open(os.path.join(__location__,'input.txt'), 'r',encoding="utf-8")
 
+sum=0
+while 1:
+    line1 = str(file.readline().replace("\n",""))
+    line2 = str(file.readline().replace("\n",""))
+    line3 = str(file.readline().replace("\n",""))
+    if not line1:
+        break
+    for i in line1:
+        if i in line2 and i in line3:
+            if ord(i)>96:
+                sum+=ord(i)-96
+            else:
+                sum+=ord(i)-64+26
+            break
+print(sum)
