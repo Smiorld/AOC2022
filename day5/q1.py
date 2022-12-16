1370ac69af6e63d0628cdad6f145a04e6038e607
Julius <q1541599745@gmail.com>
2022-12-16 14:41:27 +0000

    day5 added

---
+++ day5/q1.py
@@ -0,0 +1,32 @@
+import os
+import re
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+file = open(os.path.join(__location__,'input.txt'), 'r',encoding="utf-8")
+l=[
+    [],
+    ['F','C','J','P','H','T','W'],
+    ['G','R','V','F','Z','J','B','H'],
+    ['H','P','T','R'],
+    ['Z','S','N','P','H','T'],
+    ['N','V','F','Z','H','J','C','D'],
+    ['P','M','G','F','W','D','Z'],
+    ['M','V','Z','W','S','J','D','P'],
+    ['N','D','S'],
+    ['D','Z','S','F','M']
+]
+
+for i in range(10):
+    file.readline()
+for line in file:
+    num=re.split('move|from|to',line.replace("\n",""))
+    times=int(num[1])
+    from_l=int(num[2])
+    to_l=int(num[3])
+    for i in range(times):
+        l[to_l].append(l[from_l].pop())
+
+output=''
+for i in range(1,10):
+    output+=str(l[i].pop())
+print(output)
